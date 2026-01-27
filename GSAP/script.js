dc7bb1373a12ed1eec5f3d144442ece160e99d81
Tanmay Vijay Sherkar <sherkartanmay@gmail.com>
2026-01-27 21:08:03 +0530

    Designed a broken text animation for 'TANMAY01-D3V'

--- GSAP/script.js
+++ GSAP/script.js
@@ -120,3 +120,41 @@ menu.addEventListener("click", function () {
 cross.addEventListener("click", function () {
     tl.reverse()
 })
+
+function breakTheText(){
+    var h2 = document.querySelector("h2")
+    var h2Text = h2.textContent
+    
+    var splittedText = h2Text.split("")
+    var halfValue = splittedText.length/2
+
+    var clutter = ""
+
+    splittedText.forEach(function (elem, idx){
+        if (idx<halfValue){
+            clutter += `<span class="a">${elem}</span>`
+        }else{
+           clutter += `<span class="b">${elem}</span>`
+        }
+    })
+
+    h2.innerHTML = clutter
+}
+
+breakTheText()
+
+gsap.from("#nav h2 .a",{
+    y:80,
+    duration: 1,
+    delay:0.5,
+    stagger: 0.2,
+    opacity:0,
+})
+
+gsap.from("#nav h2 .b",{
+    y:80,
+    duration: 1,
+    delay:0.5,
+    stagger: -0.2,
+    opacity:0,
+})
